78d1e5cf842420e0c0721ad026be52e78830db55
wangqianzhen <1024611042@qq.com>
2024-10-14 18:31:16 +0800

    first commit

---
+++ Source/Axe/Axe.Build.cs
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class Axe : ModuleRules
+{
+	public Axe(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput",
+			"UnLua"
+		});
+	}
+}
---
+++ Source/Axe/Axe.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Axe.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Axe, "Axe" );
+ 
